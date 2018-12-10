ea6bf70df7932bbf59ba28504ddb1496e04d9ff7
Panu Oksiala <panu@oksiala.fi>
2018-12-10 02:43:07 +0200

    4-06

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -15,8 +15,31 @@ const favoriteBlog = blogs => {
   return {likes, title, author}
 }
 
+const mostBlogs = blogs => {
+  const authorMap = new Map()
+  const authorSet = new Set()
+  blogs.forEach(blog => {
+    const author = blog.author
+    if (!authorSet.has(author)) {
+      authorSet.add(author)
+      authorMap.set(author, 0)
+    }
+    authorMap.set(author, authorMap.get(author) + 1)
+  })
+
+  const author = [...authorSet].reduce((best, current) => {
+    return authorMap.get(best) < authorMap.get(current) ? current : best
+  })
+
+  return {
+    author, 
+    blogs: authorMap.get(author)
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 }
