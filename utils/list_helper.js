2aa8319536d81e18536bc3356e6f842fdfccb9c4
Panu Oksiala <panu@oksiala.fi>
2018-12-10 02:31:38 +0200

    4-05

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,12 +1,22 @@
-const dummy = (blogs) => {
+const dummy = blogs => {
   return 1
 }
 
-const totalLikes = (blogs) => {
+const totalLikes = blogs => {
   return blogs.reduce((acc, b) => acc + b.likes, 0)
+}
+
+const favoriteBlog = blogs => {
+  const reducer = (bestThisFar, current) => {
+    return bestThisFar.likes < current.likes ? current : bestThisFar
+  }
+
+  const {likes, title, author} = blogs.reduce(reducer)
+  return {likes, title, author}
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
 }
